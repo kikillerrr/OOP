2a1055439c3a6f0793a074173b71c95a2de2e838
kikillerrr <vikamamadova2.2@gmail.com>
2026-09-22 02:13:17 +0500

    Я создала функцию printArray, которая выводит массив на экран,и я использовала range-based for цикл и auto

--- lab_1.cpp
+++ lab_1.cpp
@@ -10,6 +10,14 @@ void fillArray(int (&arr)[10])
         std::cin >> x;
     }
 }
+
+void printArray(const int (&arr)[10])
+{
+    for (auto x : arr)
+    {
+        std::cout<< x << " ";
+    }
+}
  
 int main()
 {
@@ -19,5 +27,11 @@ int main()
     int arr[10];
 
     fillArray(arr);
+
+    std::cout << "Вот такой массив ";
+    printArray(arr);
+
+
     return 0;
+
 }
