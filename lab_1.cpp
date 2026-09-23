aa368ebae176a8e28c5c050d4cf39033f72ccf2e
kikillerrr <vikamamadova2.2@gmail.com>
2026-09-23 12:02:31 +0500

    Создала функцию swapElements, которвя меняет элементы местами

--- lab_1.cpp
+++ lab_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <utility> 
 
 void fillArray(int (&arr)[10])
 {
@@ -18,6 +19,11 @@ void printArray(const int (&arr)[10])
         std::cout<< x << " ";
     }
 }
+
+void swapElements(int (&arr)[10], int num1, int num2)
+{
+    std::swap(arr[num1], arr[num2]);
+}
  
 int main()
 {
@@ -31,6 +37,11 @@ int main()
     std::cout << "Вот такой массив ";
     printArray(arr);
 
+    std::cout << std::endl;
+
+    swapElements(arr, 0, 1);
+    std::cout << "После обмена: ";
+    printArray(arr);
 
     return 0;
 
