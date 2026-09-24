6733ca7694b7980fde4c26c428f2979874382326
kikillerrr <vikamamadova2.2@gmail.com>
2026-09-24 14:15:25 +0500

    Я объявила SafeArray

---
+++ lab_3.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+/** 
+ * @struct SafeArray 
+ * @brief Хранит динамический массив и его размер.
+ */
+
+struct SafeArray
+{
+    int* data;
+    int size;
+};
+
+
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    return 0;
+}
+
