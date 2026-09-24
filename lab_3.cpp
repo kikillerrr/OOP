5578d4d28c4b6069865e75e1e991f180f12eee14
kikillerrr <vikamamadova2.2@gmail.com>
2026-09-24 14:26:22 +0500

    Добавила функцию createArray для создания SafeArray

--- lab_3.cpp
+++ lab_3.cpp
@@ -9,6 +9,7 @@ using namespace std;
  * @brief Хранит динамический массив и его размер.
  */
 
+
 struct SafeArray
 {
     int* data;
@@ -16,11 +17,33 @@ struct SafeArray
 };
 
 
+/**
+ * @brief Создаёт динамический массив.
+ * @param size Размер массива.
+ * @return Структура SafeArray с выделенной памятью.
+ */
+
+SafeArray createArray(int size)
+{
+    SafeArray arr;
+
+    arr.data = new int[size]{};
+    arr.size = size;
+
+    return arr;
+}
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+
+    int size{};
+    
+    cout << "Введите размер массива: ";
+    cin >> size;
+    
+    SafeArray myArr = createArray(size);
     
     return 0;
 }
